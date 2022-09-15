b30e93a734b82f3301e080d1625e0cea12697ded
utquattro <utquattro@gmail.com>
2022-09-15 17:19:47 +0300

    перетащил авторизацию в бефор

--- firstbox/src/test/java/ru/stqa/pfd/firstbox/GroupCreationTests.java
+++ firstbox/src/test/java/ru/stqa/pfd/firstbox/GroupCreationTests.java
@@ -14,16 +14,17 @@ public class GroupCreationTests {
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
-    wd.get("http://192.168.31.129/addressbook/");
+    wd.get("http://192.168.31.129/addressbook/group.php");
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
+
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.id("content")).click();
